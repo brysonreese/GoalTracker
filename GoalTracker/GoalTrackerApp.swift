a016f3fbec76f89f6c85945de7bfec900ce26804
Bryson Reese <bryson@brysonreese.com>
2026-02-09 15:05:42 -0800

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by Bryson Reese on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
